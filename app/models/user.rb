d354abd63e62e3e7f5a62f426b1270bf6826d580
Jens <jens@ist-ur.org>
2018-11-09 11:02:06 +0100

    the correct password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :password_digest, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}
   validates :email, presence: true, uniqueness: {case_sensitive: false}, format: {with: VALID_EMAIL_REGEX}
   validates :name, presence: true, length: {minimum: 4, maximum: 30}, uniqueness: true
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe User, type: :model do
   # ensure name, email and password_digest are present before save
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
-  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:password) }
   it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
-  it { should validate_length_of(:password_digest).is_at_least(6) }
+  it { should validate_length_of(:password).is_at_least(6) }
   it { should validate_uniqueness_of(:name).ignoring_case_sensitivity }
   it { should validate_length_of(:name).is_at_least(4) }
 end

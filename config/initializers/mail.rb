74c478092f052546a6eaa6b47db9616f87f57806
Jens <jens@ist-ur.org>
2018-12-30 17:22:20 +0100

    mail gschisti

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
     address: "smtp.googlemail.com",
     port: 587,
     domain: "service-b.org",
-    user_name: "info@service-b.org",
-    password: "******",
+    user_name: ENV['MAIL_USER'],
+    password: ENV['MAIL_PW'],
     authentication: :plain,
     enable_starttls_auto: true
 }

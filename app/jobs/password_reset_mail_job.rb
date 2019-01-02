b4642fe99fef31ad252cf8688ff882ba69e47a47
Jens <jens@ist-ur.org>
2019-01-02 15:53:12 +0100

    default reset url

--- app/jobs/password_reset_mail_job.rb
+++ app/jobs/password_reset_mail_job.rb
@@ -2,7 +2,6 @@ class PasswordResetMailJob < ApplicationJob
   queue_as :default
 
   def perform(user, reset_token)
-    puts user.inspect
     user.send_password_reset_email(reset_token)
   end
 end

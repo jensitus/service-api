3a19539c663e65ef5fd039e2f9fa7d110456b32f
Jens <jens@ist-ur.org>
2019-01-05 18:37:38 +0100

    ENV

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -18,10 +18,6 @@ class UserMailer < ApplicationMailer
   #
   def password_reset(user, reset_token)
     puts reset_token.inspect
-    puts "user_name:"
-    puts ENV["MAIL_USER"]
-    puts "password:"
-    puts ENV["MAIL_PW"]
     @reset_token = reset_token
     @greeting = "Hi " + user.name
     @user = user

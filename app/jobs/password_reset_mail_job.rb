9e6d32e4a528fc5f9c5033018cb8bb6151df30d4
Jens <jens@ist-ur.org>
2019-01-05 23:28:24 +0100

    env jens

--- app/jobs/password_reset_mail_job.rb
+++ app/jobs/password_reset_mail_job.rb
@@ -2,10 +2,6 @@ class PasswordResetMailJob < ApplicationJob
   queue_as :default
 
   def perform(user, reset_token)
-    puts "user_name:"
-    puts ENV["MAIL_USER"]
-    puts "password:"
-    puts ENV["MAIL_PW"]
     user.send_password_reset_email(reset_token)
   end
 end
